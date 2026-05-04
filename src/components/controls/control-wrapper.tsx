37f83d8abdcf2af73e9bbb4b2287b33c4947b996
Akshat Malviya <ancientfuture1337@gmail.com>
2026-05-05 04:49:23 +0900

    Fix generation display min-width.

--- src/components/controls/control-wrapper.tsx
+++ src/components/controls/control-wrapper.tsx
@@ -7,7 +7,7 @@ export default function ControlWrapper({
 }) {
   return (
     <Card className="card mt-4 shadow-lg">
-      <div className="flex flex-row items-center gap-4">{children}</div>
+      <div className="flex items-center gap-4">{children}</div>
     </Card>
   );
 }
